94029c2c6de3ee8407bb546b8d94df659490ad57
wissallwiss <147505410+wissallwiss@users.noreply.github.com>
2024-02-28 09:32:01 +0100

    Dolphin.java

---
+++ Dolphin.java
@@ -0,0 +1,27 @@
+package tn.esprit.gestionzoo.entitees;
+
+
+public class Dolphin extends AquaticAnimal {
+    private float swimmingSpeed;
+
+    public Dolphin(String family, String name, int age, boolean isMammal, String habitat, float swimmingSpeed) {
+        super(family, name, age, isMammal, habitat);
+        this.swimmingSpeed = swimmingSpeed;
+    }
+
+    public float getSwimmingSpeed() {
+        return swimmingSpeed;
+    }
+
+    public void setSwimmingSpeed(float swimmingSpeed) {
+        this.swimmingSpeed = swimmingSpeed;
+    }
+    public void swim() {
+        System.out.println("This dolphin is swimming.");
+    }
+    @Override
+    public String toString() {
+        return super.toString() + ", swimmingSpeed=" + swimmingSpeed;
+    }
+
+}
