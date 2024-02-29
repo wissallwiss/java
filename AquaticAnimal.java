45dbb93d7f314c4733670d3b416d2f1368f2203e
wissallwiss <147505410+wissallwiss@users.noreply.github.com>
2024-02-29 11:11:41 +0100

    Update AquaticAnimal.java

--- AquaticAnimal.java
+++ AquaticAnimal.java
@@ -1,10 +1,8 @@
 package tn.esprit.gestionzoo.entitees;
 
-public  class AquaticAnimal extends Animal {
+public abstract class AquaticAnimal extends Animal {
     private String habitat;
-    public AquaticAnimal() {
 
-    }
     public AquaticAnimal(String family, String name, int age, boolean isMammal, String habitat) {
         super(family, name, age, isMammal);
         this.habitat = habitat;
@@ -17,9 +15,32 @@ public  class AquaticAnimal extends Animal {
     public void setHabitat(String habitat) {
         this.habitat = habitat;
     }
-    public void swim() {
-        System.out.println("This aquatic animal is swimming.");
+
+    // Méthode swim() déclarée comme abstraite
+    public abstract void swim();
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        AquaticAnimal other = (AquaticAnimal) obj;
+        return getName().equals(other.getName()) &&
+                getAge() == other.getAge() &&
+                habitat.equals(other.getHabitat());
     }
+
+    @Override
+    public int hashCode() {
+        int result = getName().hashCode();
+        result = 31 * result + getAge();
+        result = 31 * result + habitat.hashCode();
+        return result;
+    }
+
     @Override
     public String toString() {
         return super.toString() + ", habitat=" + habitat;
