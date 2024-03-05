3963596da3b1ac3aa1e2d9919805fa5f35719374
wissallwiss <147505410+wissallwiss@users.noreply.github.com>
2024-03-05 22:33:54 +0100

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,23 +1,62 @@
+
+package tn.esprit.gestionzoo.entitees;
+
 public class Animal {
+    private String family; // L'attribut est maintenant privé
+    private String name;
+    private int age;
+    private boolean isMammal;
 
-    String family, name;
-    int age;
-    boolean isMammal;
-
-    public Animal() {
-
-    }
-
+    // Constructeur de la classe tn.esprit.gestionzoo.entitees.Animal
     public Animal(String family, String name, int age, boolean isMammal) {
         this.family = family;
         this.name = name;
-        this.age = age;
+        setAge(age); // Utilisation de la méthode setAge pour garantir un âge non négatif
         this.isMammal = isMammal;
     }
+    class InvalidAgeException extends Exception {
+        public InvalidAgeException(String message) {
+            super(message);
+        }
+    }
+    public Animal(int age) throws InvalidAgeException {
+        if (age < 0) {
+            throw new InvalidAgeException("L'âge de l'animal ne peut pas être négatif.");
+        }
+        this.age = age;
+    }
+}
+
+    public boolean isMammal() {
+        return isMammal;
+    }
+    public void setMammal(boolean mamal){
+        isMammal=mamal;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    // Méthode publique pour définir l'âge avec une vérification de non-négativité
+    public void setAge(int age) {
+        if (age >= 0) {
+            this.age = age;
+        } else {
+            System.out.println("L'âge ne peut pas être négatif. La valeur par défaut de 0 sera utilisée.");
+
+        }
+    }
+
+    // Méthode publique pour obtenir l'âge
+    public int getAge() {
+        return age;
+    }
+
+    // Autres getters et setters pour les attributs name, family et isMammal
 
     @Override
     public String toString() {
-        return "Animal{" +
+        return "tn.esprit.gestionzoo.entitees.Animal{" +
                 "family='" + family + '\'' +
                 ", name='" + name + '\'' +
                 ", age=" + age +
